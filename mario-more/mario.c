60d307849726571ddc3f0f5150aff3acc8ba532f
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-01-28 08:58:27 +0000

    Sun, Jan 28, 2024, 4:58 PM +08:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,12 +5,15 @@ int main(int n)
 {
     int num = get_int("");
 
+
+}
+
+
+void fun(int n)
+{
     for(int i, i < n, i--)
     {
         printf(" " * (num - 1) + "#" + "  " + " " * (num - 1)  + "\n");
         num --;
     }
 }
-
-
-void fun()
