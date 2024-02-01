3c065676a0580b1e94dcc6f2e3da996003cd7aac
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-01 10:31:22 +0000

    Thu, Feb 1, 2024, 6:31 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,7 +6,12 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("");
-        return 1
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if ()
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
     }
 }
