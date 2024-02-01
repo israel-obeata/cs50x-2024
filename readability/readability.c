e6f2eaf345b5d60d849639530375a5395fc118e8
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-01 09:56:16 +0000

    Thu, Feb 1, 2024, 5:56 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -31,5 +31,17 @@ int main(void)
     float L = letters / (float) words * 100;
     float S = words / (float) sentences * 100;
     int index = round(0.0588 * L - 0.296 * S - 15.8);
-    printf("");
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
