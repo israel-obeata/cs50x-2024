afcf2a890f9a34702252266dbd3a9c455749f2f4
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-02 11:08:24 +0000

    Fri, Feb 2, 2024, 7:08 PM +08:00

--- test.c
+++ test.c
@@ -1,11 +1,9 @@
 #include <cs50.h>
-#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-    string str = get_string("please input: ");
+    string str = get_string("my: ");
     printf("%c,%c,%c,%c|\n", str[0], str[1], str[2], str[3]);
     printf("%i,%i,%i,%i|\n", str[0], str[1], str[2], str[3]);
 }
