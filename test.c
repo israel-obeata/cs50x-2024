e37ba503e2e945d68c19ccfc99382e7e2391b3fb
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-02 11:01:39 +0000

    Fri, Feb 2, 2024, 7:01 PM +08:00

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string str = get_string("");
+    printf("%c,%c,%c,%c,\n", str[0], str[1], str[2], str[3]);
+    printf("%i,%i,%i,%i\n", str[0], str[1], str[2], str[3]);
+}
