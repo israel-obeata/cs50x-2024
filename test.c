e1ce188ab7deaca3f5a7c3201ed9511fcab93180
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-07 10:31:55 +0000

    Wed, Feb 7, 2024, 6:31 PM +08:00

--- test.c
+++ test.c
@@ -1,9 +1,26 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    string str = get_string("my: ");
-    printf("%c,%c,%c,%c|\n", str[0], str[1], str[2], str[3]);
-    printf("%i,%i,%i,%i|\n", str[0], str[1], str[2], str[3]);
+    // string str = get_string("my: ");
+    // printf("%c,%c,%c,%c|\n", str[0], str[1], str[2], str[3]);
+    // printf("%i,%i,%i,%i|\n", str[0], str[1], str[2], str[3]);
+
+    int height = get_int("Height: ");
+    draw(height);
+}
+
+void draw(int n)
+{
+    if (n <= 1)
+    {
+        printf("#");
+    }
+    else
+    {
+        draw(n - 1);
+    }
 }
