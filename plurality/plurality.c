2b98823dc0c48526c28bb53cd23563ea10488c19
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-08 08:13:35 +0000

    Thu, Feb 8, 2024, 4:13 PM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -82,7 +82,7 @@ void print_winner(void)
     // TODO
     int max_votes = 0;
     int max_index = 0;
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 1; i < argc; i++)
     {
         if ()
         {
