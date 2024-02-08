24d908f93287ff53c3feb729442bd7af29d29595
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-08 13:44:42 +0000

    Thu, Feb 8, 2024, 9:44 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -140,7 +140,10 @@ void add_pairs(void)
     {
         for(int j = 0; j < candidate_count; j++)
         {
+            if (preferences[i][j] > preferences[j][i]) // pre[0][2] > pre[2][0]
+            {
 
+            }
         }
     }
     return;
