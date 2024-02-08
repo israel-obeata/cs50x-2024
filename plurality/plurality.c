3b783ae7dbff013b8fa0d034c6f4d61680cbb615
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-08 08:12:32 +0000

    Thu, Feb 8, 2024, 4:12 PM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], name) == 0)
         {
