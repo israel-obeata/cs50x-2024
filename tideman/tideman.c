79c865a13ff0f83e834f0b274de9d002f079077a
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-08 13:42:14 +0000

    Thu, Feb 8, 2024, 9:42 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -136,6 +136,13 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        for(int j = 0; j < candidate_count; j++)
+        {
+
+        }
+    }
     return;
 }
 
