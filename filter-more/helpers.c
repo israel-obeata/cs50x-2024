0d5d5c19f9078d0e53a22150c8fdb6c8cf5b194a
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-16 12:39:47 +0000

    Fri, Feb 16, 2024, 8:39 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,6 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float red = image[i][j].rgbtRed;
+            float green = image[i][j].rgbtGreen;
+            float blue = image[i][j].rgbtBlue;
+
+            int average = round((red + green + blue) / 3);
+        }
+    }
     return;
 }
 
