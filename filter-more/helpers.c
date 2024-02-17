95a7b8c61bf820c62ed90087eb4356aa02989d7c
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-17 05:26:04 +0000

    Sat, Feb 17, 2024, 1:26 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -89,9 +89,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            temp[i][j].rgbtRed = image[i][j].rgbtRed;
-            temp[i][j].rgbtGreen = image[i][j].rgbtGreen;
-            temp[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
         }
     }
 }
