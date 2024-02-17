4c83eeba8221376787bc9dd66c7162b4451a78a2
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-17 11:10:24 +0000

    Sat, Feb 17, 2024, 7:10 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,16 +3,20 @@
 
 int main(int argc, char *argv[])
 {
+    // Accept a single command-line argument
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
 
+    // Open the memory card
     FILE *card = fopen(argv[1], "r");
     if (card == NULL)
     {
         printf("Could not open file");
         return 2;
     }
+
+    
 }
