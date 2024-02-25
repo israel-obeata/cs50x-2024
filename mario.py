d63770a6b43c50edf3b7a88c5aaf3c7292f7ff89
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-25 13:54:03 +0000

    Sun, Feb 25, 2024, 9:54 PM +08:00

--- mario.py
+++ mario.py
@@ -1,12 +1,11 @@
 from cs50 import get_int
 
 while True:
-    try:
+
         h = get_int("Height: ")
         if h > 0:
             break
-    except ValueError:
-        pass
+
 
 def main(h):
     for i in range(h)
