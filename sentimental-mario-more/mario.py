0b7874e2e91580f22276a5444f3130ea9e3c9815
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-25 15:44:18 +0000

    Sun, Feb 25, 2024, 11:44 PM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,6 +5,6 @@ while h < 1 or h > 8:
     h = get_int("Height: ")
 
 for i in range(h):
-    blank = " " * (h -1 - i)
+    blank = " " * (h - 1 - i)
     s = "#" * (i + 1)
     print(f"{blank}{s}  {s}")
