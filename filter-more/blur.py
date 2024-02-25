3b72e747cb0fa89faa12be7c83781099c48a0268
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-25 06:13:40 +0000

    Sun, Feb 25, 2024, 2:13 PM +08:00

--- filter-more/blur.py
+++ filter-more/blur.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFilter
 
-before = Image.open("bridge.bmp")
+before = Image.open("images/yard.bmp")
 after = before.filter(ImageFilter.BoxBlur(10))
 after.save("out.bmp")
