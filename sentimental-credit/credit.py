3b5910d28ce1b0d086beb72c2a824acbc10a5dd3
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-26 12:21:48 +0000

    Mon, Feb 26, 2024, 8:21 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,17 +1,18 @@
 from cs50 import get_int
 
+
 def main():
-    while True:
-        num = input("Number: ")
-        if check_valid(num):
-            print_brand(num)
-            break
+    num = input("Number: ")
+    if check_valid(num):
+        print_brand(num)
+    else:
+        print("INVALID")
+
 
 def check_valid(n):
     length = len(n)
     if (length == 13 or length == 15 or len == 16) and check_sum(n):
         return True
-    print("INVALID")
     return False
 
 def check_sum(n):
