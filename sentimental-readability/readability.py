e7e8d5add097d68aaed5d34fb9db724c6e8e5094
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-02-27 06:55:21 +0000

    Tue, Feb 27, 2024, 2:55 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,10 +8,28 @@ def main():
 
 
 def calculate(s):
-    letters = words = sentences = 0
+    letters = sentences = 0
+    words = 1
     for i in range(len(s)):
         if s[i].isalpha():
-            letters
+            letters += 1
+        elif s[i].isspace():
+            words += 1
+        elif s[i] in ['.', '!', '?']:
+            sentences += 1
+
+    L = letters / words * 100
+    S = sentences / words * 100
+    return round(0.0588 * L - 0.296 * S - 15.8)
 
 
-def print_grade(n)
+def print_grade(n):
+    if n < 1:
+        print("Before Grade 1")
+    elif n >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {n}")
+
+
+main()
