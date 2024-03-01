96fe365acfb6b274bfd4c269ade2e78cc3ec055e
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 13:20:16 +0000

    Fri, Mar 1, 2024, 9:20 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+
+-- Find crime scene description
 SELECT description
   FROM crime_scene_reports
  WHERE month = 7 AND day = 28 AND year = 2023
