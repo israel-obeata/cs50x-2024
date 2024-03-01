5a40995c1dc93d775809ae314f92da37a535e1ac
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 14:08:27 +0000

    Fri, Mar 1, 2024, 10:08 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,7 +19,7 @@ SELECT name, transcript
 SELECT activity, license_plate
   FROM bakery_security_logs
  WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10
-   AND minute >= 15 AND minute <= 25;
+   AND minute > 15 AND minute < 25;
 
  -- Find the thief's flight
 
