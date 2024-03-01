12b479f06b7f5e2f17ef7c7d51b159a8f732a73d
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 11:19:58 +0000

    Fri, Mar 1, 2024, 7:19 PM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,7 +2,7 @@ SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE people.name = 'Bradley Cooper' AND title IN (
-    SELECT title, name FROM movies
+    SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
     WHERE people.name="Jennifer Lawrence"
