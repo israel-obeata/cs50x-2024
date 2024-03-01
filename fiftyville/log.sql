9fb58d0753d0ca1d24586bb87f4ad3d9b365d94b
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 13:13:50 +0000

    Fri, Mar 1, 2024, 9:13 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description
   FROM crime_scene_reports
- WHERE month = 7 AND 
+ WHERE month = 7 AND day = 28 AND year = 2023
+   AND street = 'Humphrey Street';
