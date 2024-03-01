2e0c13081c68bae83b10da58ef1a01ba28a4ce70
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 13:47:11 +0000

    Fri, Mar 1, 2024, 9:47 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,17 +1,19 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Find crime scene description
+-- We found out where and when the burglary took place, and that there were three witnesses
+-- [bakery, 10:15, 3 witnesses]
 SELECT description
   FROM crime_scene_reports
  WHERE year = 2023 AND month = 7 AND day = 28
    AND street = 'Humphrey Street';
--- We found out where and when the burglary took place, and that there were three witnesses
--- bakery, 10:15, 3 witnesses
 
 -- Find the transcripts of interviews
+-- 
 SELECT name, transcript
   FROM interviews
  WHERE year = 2023 AND month = 7 AND day = 28;
+ --
 
 
 -- Find the car entry and exit information for the bakery
