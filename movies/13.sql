c3cd8fda0efd48cf4fc36e8b83cb2e54ba9b5374
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 11:33:03 +0000

    Fri, Mar 1, 2024, 7:33 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,9 @@
 SELECT name FROM peoples
 JOIN stars ON peoples.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE 
+WHERE movie IN (
+    SELECT movie FROM movies
+    JOIN stars ON peoples.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
+    WHERE peoples.name = 'Kevin Bacon' AND peoples.birth = 1958
+)
