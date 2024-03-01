f5aa7c3e2c2ab9e9aac5c48279a744ec2022b017
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 10:50:28 +0000

    Fri, Mar 1, 2024, 6:50 PM +08:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,5 @@
 SELECT title FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
-JOIN 
+JOIN stars ON ratings.movie_id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE people.name = 'Chadwick Boseman'
