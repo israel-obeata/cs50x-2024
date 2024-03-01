c5937f9cdc5befc2061df9ad30b21620853bbb81
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 13:58:41 +0000

    Fri, Mar 1, 2024, 9:58 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,19 +1,16 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Find crime scene description
--- We found out where and when the burglary took place, and that there were three witnesses
--- [bakery, 10:15, 3 witnesses]
+-- 1. bakery    2. 10:15   3. witnesses
 SELECT description
   FROM crime_scene_reports
  WHERE year = 2023 AND month = 7 AND day = 28
    AND street = 'Humphrey Street';
 
 -- Find the transcripts of interviews
--- 1. We found out, within 10 minutes of the theft, the car left the bakery.
--- 2. The thief was seen at the ATM on Leggett Street before going to the bakery.
--- 3. When the thief left the bakery, he called a man and spoke for less than a minute.
---    And ask that person to purchase the earliest flight for the next day
--- [10:15-10:25, ATM on Leggett Street, the earliest flight for the next day
+-- 1. exit the bakery between 10:15-10:25
+-- 2. ATM on Leggett Street before 10:15
+-- 3. the earliest flight for the next day
 SELECT name, transcript
   FROM interviews
  WHERE year = 2023 AND month = 7 AND day = 28;
