c04fa53b7a87441b7f162009684d6fbd9c06fa5c
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 11:39:01 +0000

    Fri, Mar 1, 2024, 7:39 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT(name) FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE name != 'Kevin Bacon' AND movies.title IN (
-    SELECT title FROM movies
+WHERE name != 'Kevin Bacon' AND movies.id IN (
+    SELECT movies.id FROM movies
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON stars.movie_id = movies.id
     WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
