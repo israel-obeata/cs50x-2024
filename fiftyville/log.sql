c0a7d402ec6119a296fdacee49e4badbe0fe6e3e
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 13:23:31 +0000

    Fri, Mar 1, 2024, 9:23 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,3 +5,9 @@ SELECT description
   FROM crime_scene_reports
  WHERE month = 7 AND day = 28 AND year = 2023
    AND street = 'Humphrey Street';
+
+-- Find bakery security log
+SELECT description
+  FROM crime_scene_reports
+ WHERE month = 7 AND day = 28 AND year = 2023
+   AND street = 'Humphrey Street';
