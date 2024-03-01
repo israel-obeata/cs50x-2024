bd0c59380f8c1f651d80da56f7bf48b669b8dd25
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 14:20:07 +0000

    Fri, Mar 1, 2024, 10:20 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -15,13 +15,8 @@ SELECT name, transcript
   FROM interviews
  WHERE year = 2023 AND month = 7 AND day = 28;
 
--- Find ATM info
+-- Find account_number and amount
 SELECT account_number, amount FROM atm_transactions
- WHERE atm_location = 'Leggett Street'
-   AND year = 2023 AND month = 7 AND day = 28
-   AND transaction_type = 'withdraw';
-
-SELECT * FROM atm_transactions
  WHERE atm_location = 'Leggett Street'
    AND year = 2023 AND month = 7 AND day = 28
    AND transaction_type = 'withdraw';
