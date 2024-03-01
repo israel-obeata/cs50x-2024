59855b06db4adc8a3407d21423fe981249975001
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 10:54:44 +0000

    Fri, Mar 1, 2024, 6:54 PM +08:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,4 @@
-SELECT title, rating FROM movies
+SELECT title FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
 JOIN stars ON ratings.movie_id = stars.movie_id
 JOIN people ON stars.person_id = people.id
