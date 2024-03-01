0407507a8139302b0154dcd979e9297f81946792
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-01 13:36:21 +0000

    Fri, Mar 1, 2024, 9:36 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,7 +11,8 @@ SELECT activity, license_plate
   FROM bakery_security_logs
  WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10;
 
--- Find
+-- Find the transcript for interviews
+SELECT transcript 
 
 
 
