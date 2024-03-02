a2b46f4d711fa4e185dfcc6e764f296ed6ad7a89
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-02 07:53:48 +0000

    Sat, Mar 2, 2024, 3:53 PM +08:00

--- fiftyville/test.sql
+++ fiftyville/test.sql
@@ -1,7 +1,7 @@
 SELECT people.name FROM people
   JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
-  JOIN phone_calls ON people.name = phone_calls.caller
-  JOIN passengers ON people.passport_number = passengers.passport_number
+  --JOIN phone_calls ON people.name = phone_calls.caller
+  --JOIN passengers ON people.passport_number = passengers.passport_number
   --JOIN bank_accounts ON people.id = bank_accounts.person_id
   --JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
  WHERE bakery_security_logs.year = 2023 AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28
