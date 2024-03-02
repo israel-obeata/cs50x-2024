da386c923728b2449ac8ac2896419c289958ebb4
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-02 05:52:15 +0000

    Sat, Mar 2, 2024, 1:52 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -52,7 +52,7 @@ SELECT people.name FROM people
   JOIN phone_calls ON people.name = phone_calls.caller
   JOIN passengers ON people.passport_number = passengers.passport_number
   JOIN bank_accounts ON people.id = bank_accounts.person_id
-  JOIN atm_transactions AS atm ON bank_accounts.account_number = atm_transactions.account_number
+  JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
  WHERE people.license_plate IN
        (SELECT license_plate FROM bakery_security_logs
          WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10
