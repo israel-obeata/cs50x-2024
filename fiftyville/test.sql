c0c0c20b29cbc183fd7071d579d51204f11b4ded
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-02 06:18:32 +0000

    Sat, Mar 2, 2024, 2:18 PM +08:00

--- fiftyville/test.sql
+++ fiftyville/test.sql
@@ -6,11 +6,11 @@ SELECT people.name FROM people
   JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
  WHERE bakery_security_logs.year = 2023 AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28
    AND bakery_security_logs.hour = 10
-   AND bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25
-   AND bakery_security_logs.activity = 'exit'
-   AND phone_calls.year = 2023 AND phone_calls.month = 7 AND phone_calls.day = 28
-   AND phone_calls.duration <= 60
-   AND passengers.flight_id = 36
-   AND atm_transactions.atm_location = 'Leggett Street'
-   AND atm_transactions.year = 2023 AND atm_transactions.month = 7 AND atm_transactions.day = 28
-   AND atm_transactions.transaction_type = 'withdraw';
+   --AND bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25
+   --AND bakery_security_logs.activity = 'exit'
+   --AND phone_calls.year = 2023 AND phone_calls.month = 7 AND phone_calls.day = 28
+   --AND phone_calls.duration <= 60
+   --AND passengers.flight_id = 36
+   --AND atm_transactions.atm_location = 'Leggett Street'
+   --AND atm_transactions.year = 2023 AND atm_transactions.month = 7 AND atm_transactions.day = 28
+   --AND atm_transactions.transaction_type = 'withdraw';
