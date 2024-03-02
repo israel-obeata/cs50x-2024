eb7a5bc4f8efb3c9b312c06f386ce420e5cd914c
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-02 08:26:25 +0000

    Sat, Mar 2, 2024, 4:26 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -24,40 +24,19 @@ SELECT id, origin_airport_id, destination_airport_id FROM flights
 
 -- Find the info of airports and cities
 -- 8  | CSF  | Fiftyville Regional Airport | Fiftyville
--- 4  | LGA  | LaGuardia Airport           | New York City
+-- 4  | LGA  | LaGuardia Airport           | New York City ★★★
 SELECT * FROM airports
  WHERE id = 8 OR id = 4;
 
 
-
--- Find account_number and amount
-SELECT account_number, amount FROM atm_transactions
- WHERE atm_location = 'Leggett Street'
-   AND year = 2023 AND month = 7 AND day = 28
-   AND transaction_type = 'withdraw';
-
--- Find the entry and exit information of the bakery
-SELECT activity, license_plate
-  FROM bakery_security_logs
- WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10
-   AND minute > 15 AND minute < 25;
-
--- Find phone calls
-SELECT caller, receiver FROM phone_calls
- WHERE year = 2023 AND month = 7 AND day = 28
-   AND duration < 60
- ORDER BY phone_calls.duration;;
-
-
-
+-- Find the thief.(Bruce ★★★)
 SELECT people.name FROM people
   JOIN bakery_security_logs AS bakery ON people.license_plate = bakery.license_plate
   JOIN phone_calls AS calls ON people.phone_number = calls.caller
   JOIN passengers ON people.passport_number = passengers.passport_number
   JOIN bank_accounts AS bank ON people.id = bank.person_id
   JOIN atm_transactions AS atm ON bank.account_number = atm.account_number
- WHERE bakery.year = 2023 AND bakery.month = 7 AND bakery.day = 28
-   AND bakery.hour = 10
+ WHERE bakery.year = 2023 AND bakery.month = 7 AND bakery.day = 28 AND bakery.hour = 10
    AND bakery.minute >= 15 AND bakery.minute <= 25
    AND bakery.activity = 'exit'
    AND calls.year = 2023 AND calls.month = 7 AND calls.day = 28
