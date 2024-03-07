c507de18f9a6e90bbd61355d35ca096f875a55c3
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-07 08:45:08 +0000

    Thu, Mar 7, 2024, 4:45 PM +08:00

--- Week4-Memory/filter-more/filter.c
+++ Week4-Memory/filter-more/filter.c
@@ -1,3 +1,5 @@
+// https://cs50.harvard.edu/x/2024/psets/4/filter/more/
+
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
--- Week4-Memory/volume/volume.c
+++ Week4-Memory/volume/volume.c
@@ -1,4 +1,4 @@
-//https://cs50.harvard.edu/x/2024/psets/4/volume/
+// https://cs50.harvard.edu/x/2024/psets/4/volume/
 
 // Modifies the volume of an audio file
 
