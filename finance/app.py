6b67fb40fbe12c559a952d36b2d7992329fc3858
Uxdruh <84405924+Uxdruh@users.noreply.github.com>
2024-03-08 12:57:33 +0000

    Fri, Mar 8, 2024, 8:57 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -73,8 +73,8 @@ def buy():
             return apology("Not enough cash!")
         else:
             db.execute("UPDATE users SET cash = ? WHERE id = ?", cash - total_price, user_id)
-            db.execute("INSERT INTO transactions (user_id, type, name, shares, price, symbol) VALUES (?, ?, ?, ?, ?, ?)",
-                       user_id, "buy", item_symbol, item_price, )
+            db.execute("INSERT INTO transactions (user_id, type, name, price, shares, symbol) VALUES (?, ?, ?, ?, ?, ?)",
+                       user_id, "buy", item_symbol, item_price, shares, symbol )
 
         return redirect("/")
 
