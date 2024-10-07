ab7b8977c92834550f085e09c48f5bca4e100297
israel-obeata <Israelobeata@gmail.com>
2024-10-07 11:37:53 +0000

    Mon, Oct 7, 2024, 11:37 AM +00:00

--- average.c
+++ average.c
@@ -1,12 +1,4 @@
 #include<cs50.h>
 #include<stdio.h>
 
-int main(void){
-    int score[3];
-    int sum = 0;
-  for(int i=0;i<3;i++){
-    score[]=get_int("score");
-     sum+=score[];
-  }i
-  printf("average is %f",sum/3.0);
-}
+
