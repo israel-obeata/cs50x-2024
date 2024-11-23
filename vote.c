36a0cbe02c1589b117da422306f1ade707075b14
israel-obeata <Israelobeata@gmail.com>
2024-11-23 18:17:37 +0000

    Sat, Nov 23, 2024, 6:17 PM +00:00

--- vote.c
+++ vote.c
@@ -3,8 +3,8 @@
 
 int main(void){
     int length=get_int("voter's length: ");
-    string name= get_string("candidate: ");
     for(int i=0;i<length;i++){
-        name[]
+        string name= get_string("candidate: ");
+        name[i]= name;
     }
 }
