fe03ad6f937b96f5ff4acbf53c129c37d33522d2
israel-obeata <Israelobeata@gmail.com>
2024-11-24 20:40:08 +0000

    Sun, Nov 24, 2024, 8:40 PM +00:00

--- vote.c
+++ vote.c
@@ -1,24 +1,37 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
+// Display winner of election
+    print_winner();
+    }
 
-int main(void)
-{
-  int amount = get_int("\nHow many candidates: ");
-   string names[amount];
-   for(int i = 0; i < amount; i++){
-        names[i] = get_string(" name: ");
-        }
-      for(int j=0;j<amount;j++){
-        int n=0;
-        for(int p=0;p<amount;p++){
-          if(strcmp(names[j],names[p]) == 0){
-            n++;
-          }
-          names[j]=n;
-        }
-          printf("%d",names[3]);
-      }
-}
+    // Update vote totals given a new vote
+    bool vote(string name)
+    {
+        // TODO
+        for (int i = 0; i < candidate_count; i++)
+              {
+                    if (strcmp(candidates[i].name, name) == 0)
+                                {
+                                            candida                                                          return false;
+
+                                                                                                                   {
+                                                                            // TODO
+                                                                                int max_votes = 0;
+                                                                                    int max_index = 0;
+                                                                                        for (int i = 0; i < candidate_count; i++)
+                                                                                            {
+                                                                                                    if (candidates[i].votes >= max_votes)
+                                                                                                            {
+                                                                                                                        max_index = i;
+                                                                                                                                    max_votes = candidates[i].votes;
+                                                                                                                                            }
+                                                                                                                                                }
+                                                                                                                                                    for (int i = 0; i < candidate_count; i++)
+                                                                                                                                                        {
+                                                                                                                                                                if (candidates[i].votes == max_votes)
+                                                                                                                                                                        {
+                                                                                                                                                                                    printf("%s\n", candidates[i].name);
+                                                                                                                                                                                            }
+                                                                                                                                                                                                }
+                                                                                                                                                                                                    return;
+                                                                                                                                                                                                    }
 
 
