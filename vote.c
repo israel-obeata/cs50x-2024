db88da5c37b2fd362b98785278245a5352d7c973
israel-obeata <Israelobeata@gmail.com>
2024-11-25 13:54:12 +0000

    Mon, Nov 25, 2024, 1:54 PM +00:00

--- vote.c
+++ vote.c
@@ -1,24 +1,20 @@
 #include <cs50.h>
-#i
+#include <stdio.h>
+#include <string.h>
 
-
-
-
-
-
-                                                                                                                   {
-
-
-
-                                                                                                                                                                                    {
-                                                                                                    if (candidates[i].votes >= max_votes)
-                                                                                                            {
-                                                                                                                        max_index = i;
-                                                                                                                                    max_votes = candidates[i].votes;
-                                                                                                                                            }
-                                                                                                                                                }
-
-                                                                                                                                                        {
+int main(void){
+    string names[amount];
+  int amount= get_int("\namount of voters: ");
+  for(int i=0;i<amount;i++){
+        names[i]=get_string("names of candidates: ");
+  }
+  for(int j=0;j<amount;j++){
+        int n=0;
+        for(int p=0;p<amount;p++){
+                
+        }
+  }
+}
 
 
 
