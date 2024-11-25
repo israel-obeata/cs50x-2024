093244507a45bf1122e71dc01a876a83a8494eca
israel-obeata <Israelobeata@gmail.com>
2024-11-25 13:42:53 +0000

    Mon, Nov 25, 2024, 1:42 PM +00:00

--- vote.c
+++ vote.c
@@ -1,37 +1,30 @@
-// Display winner of election
-    print_winner();
-    }
+#include <cs50.h>
+#i
 
-    // Update vote totals given a new vote
-    bool vote(string name)
-    {
-        // TODO
-        for (int i = 0; i < candidate_count; i++)
-              {
-                    if (strcmp(candidates[i].name, name) == 0)
-                                {
-                                            candida                                                          return false;
+
+
+
+
 
                                                                                                                    {
-                                                                            // TODO
-                                                                                int max_votes = 0;
-                                                                                    int max_index = 0;
-                                                                                        for (int i = 0; i < candidate_count; i++)
-                                                                                            {
+
+
+
+                                                                                                                                                                                    {
                                                                                                     if (candidates[i].votes >= max_votes)
                                                                                                             {
                                                                                                                         max_index = i;
                                                                                                                                     max_votes = candidates[i].votes;
                                                                                                                                             }
                                                                                                                                                 }
-                                                                                                                                                    for (int i = 0; i < candidate_count; i++)
+
                                                                                                                                                         {
-                                                                                                                                                                if (candidates[i].votes == max_votes)
-                                                                                                                                                                        {
-                                                                                                                                                                                    printf("%s\n", candidates[i].name);
-                                                                                                                                                                                            }
-                                                                                                                                                                                                }
-                                                                                                                                                                                                    return;
-                                                                                                                                                                                                    }
+
+
+
+
+
+
+
 
 
