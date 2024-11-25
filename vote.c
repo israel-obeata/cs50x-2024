8143b40f011622184d0d0637142d80df8e292af6
israel-obeata <Israelobeata@gmail.com>
2024-11-25 19:58:23 +0000

    Mon, Nov 25, 2024, 7:58 PM +00:00

--- vote.c
+++ vote.c
@@ -17,10 +17,6 @@ int main(void){
                 if(strcmp(names[j],names[p])==0){
                         n++;
                 }
-                if(j<amount-1){
-                if(strcmp(names[j+1],names[p])==0){
-                  f++;
-                }
                 
         }
         }
