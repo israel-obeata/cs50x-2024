0cf304faf4bd6d2f74b9f60eb3426a2ed266c1dd
israel-obeata <Israelobeata@gmail.com>
2024-11-25 21:29:25 +0000

    Mon, Nov 25, 2024, 9:29 PM +00:00

--- vote.c
+++ vote.c
@@ -10,21 +10,19 @@ int main(void){
         names[i]=get_string("names of candidates: ");
   }
     int large = 0;
-    int g=0;
-    int f=0;
+    string winner;
   for(int j=0;j<amount;j++){
         int n=0;
         for(int p=0;p<amount;p++){
                 if(strcmp(names[j],names[p])==0){
                   n++;
-                  g++;
+
                 }
              }
              if(n > large){
                large=n;
                winner = names[j];
              }
-             f++;
   }
       printf("\n%s %d\n",winner,large);
 
